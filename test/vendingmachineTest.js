4d0243f28258300eccdbed7786bc2413197ae872
Nate Rauh <NRauh@users.noreply.github.com>
2016-01-06 01:52:02 -0600

    Handling money as cents
    Didn't think of floating point errors.
    Currency is going to be in cents now.

--- test/vendingmachineTest.js
+++ test/vendingmachineTest.js
@@ -12,33 +12,33 @@ describe("insertedAmount", function() {
 	});
 
 	it("tells the value of insertedCoinAmount variable", function() {
-		vendingMachine.insertedCoinAmount = 0.25;
+		vendingMachine.insertedCoinAmount = 25;
 		var value = vendingMachine.insertedAmount();
-		expect(value).to.equal(0.25);
+		expect(value).to.equal(25);
 	});
 });
 
 describe("addCoin", function() {
 	it("can add quarters", function() {
 		var quarter = vendingMachine.addCoin(24.26, 5.67);
-		expect(quarter).to.equal(0.25);
+		expect(quarter).to.equal(25);
 	});
 
 	it("can add dimes", function() {
 		var dime = vendingMachine.addCoin(17.91, 2.268);
-		expect(dime).to.equal(0.10);
+		expect(dime).to.equal(10);
 	});
 
 	it("can add nickels", function() {
 		var nickel = vendingMachine.addCoin(21.21, 5.0);
-		expect(nickel).to.equal(0.5);
+		expect(nickel).to.equal(5);
 	});
 
 	it("rejects other coins", function() {
 		var penny = vendingMachine.addCoin(19.05, 2.5);
-		expect(penny).to.equal(0.0);
+		expect(penny).to.equal(0);
 		var weirdCoin = vendingMachine.addCoin(15.67, 4.2);
-		expect(weirdCoin).to.equal(0.0);
+		expect(weirdCoin).to.equal(0);
 	});
 
 	it("adds the values together", function() {
--- vendingmachine.js
+++ vendingmachine.js
@@ -12,15 +12,15 @@ module.exports = {
 	addCoin: function(mmDiameter, gWeight) {
 		if (mmDiameter === 24.26 && gWeight === 5.670) {
 			this.insertedCoinAmount += 0.25;
-			return 0.25;
+			return 25;
 		} else if (mmDiameter === 17.91 && gWeight === 2.268) {
 			this.insertedCoinAmount += 0.10;
-			return 0.10;
+			return 10;
 		} else if (mmDiameter === 21.21 && gWeight === 5.0) {
-			return 0.50;
+			return 5;
 		} else {
 			// need to add function for coin return
-			return 0.0;
+			return 0;
 		}
 	}
 }
